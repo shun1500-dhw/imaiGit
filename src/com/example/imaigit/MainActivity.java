bf588378284b655c257e50bb6e3393235e38bd9b
AyumiKatayama <misaox330@gmail.com>
2013-12-23 20:27:16 +0900

    松島コミット

--- src/com/example/imaigit/MainActivity.java
+++ src/com/example/imaigit/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.Menu;
 
 /**
  * これはGitの練習のためのコメントです。
+ * 
  * @author shun
- *
+ * 
  */
 public class MainActivity extends Activity {
 
+	// お疲れ様です。松島です。
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
